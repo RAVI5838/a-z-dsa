fc6114c9b78f24af132d779395fb66d2e3f63d34
RAVI5838 <126051355+RAVI5838@users.noreply.github.com>
2023-03-30 23:10:42 +0530

    new

--- 1.2/126.cpp
+++ 1.2/126.cpp
@@ -1,12 +1,59 @@
-int start = n, end = n;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=end;j++){
-                if(j>=start && j<=end)
-                    cout << "*";
-                else
-                    cout << " ";
-            }
-            cout << endl;
-            start--;
-            end++;
+#include<iostream>
+using namespace std;
+
+void print1(int n){
+   for(int i=n; i>=1; i--){
+
+         for(int j=1; j<=i; j++){
+
+             cout<<"* ";
+
+         }
+
+         cout<<endl;
+
+     }
+
+}
+
+void print2(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 0; j <n-i+1; j++)
+    {
+      cout << j+1 <<" ";
+    } cout << endl;
+    
+  }
+  
+}
+
+void print3(int n){
+    for(int i=1;i<n;i++){
+           
+         for(int j=1;j<n-i+1;j++){
+                
+            cout << j;
+                
         }
+            cout << endl;
+           
+        }}
+        
+void print4(int n){
+
+} 
+    
+    int main() {
+    int  t;
+    cin >> t;
+    for(int i=0;i<t;i++) {
+    int n;
+    cin >> n;
+    //print1(n);
+    //print2(n);
+    //print3(n);
+    print4(n);}
+    return 0;
+}
